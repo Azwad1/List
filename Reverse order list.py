f8a75e1117ad39d0cebfb1a7a4dbdc4665671505
Azwad1 <155709985+Azwad1@users.noreply.github.com>
2024-10-15 11:09:52 +0530

    Reverse order list.py

--- Reverse order list.py
+++ Reverse order list.py
@@ -4,9 +4,8 @@ Sample Input:
 10 20 30 40 50
 Sample Output:
 50 40 30 20 10 
-10)Write a program to sort the given list and print it.
-Sample Input:
-30 20 10 50 40
-Sample Output:
-10 20 30 40 50 
+
 """
+elements = input().split()  
+reversed_elements = elements[::-1]
+print(" ".join(reversed_elements))
